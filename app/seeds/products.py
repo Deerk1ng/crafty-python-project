b087551cd990134a9b72e2739b3757966f76654c
Hayden Ruiz <haydenr1111@gmail.com>
2024-09-12 16:38:40 -0700

    modified users and started products model/migration

---
+++ app/seeds/products.py
@@ -0,0 +1,17 @@
+from app.models import db, Product, environment, SCHEMA
+from sqlalchemy.sql import text
+
+def seed_products():
+    chainmail = Product(
+        ownclearerId='2' ,name='chainmail' , price='100.00' , description='This shirt made of interlocked metal rings is perfect for any medieval reinactment outfit you are looking for. Perfect for renfaires' , category='shirts' )
+
+    db.session.add(chainmail)
+    db.session.commit()
+
+def undo_products():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.products RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM products"))
+
+    db.session.commit()
