c80efb4b0d3c62e73100f2b99b675d81f2212655
Hayden Ruiz <haydenr1111@gmail.com>
2024-09-16 20:52:17 -0700

    part 2

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,6 +7,6 @@ from .order import Order
 from .productImage import ProductImage
 from .reviewImage import ReviewImage
 from .review import Review
-from .shopping_cart import shopping_cart
+from .shopping_cart import shoppingCart
 from .cartItem import CartItem
 from .db import environment, SCHEMA
